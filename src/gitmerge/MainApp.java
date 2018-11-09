1d4ed7055df050507420cf211c1b70ab9e7e1b66
javab2i <jd370332@gmail.com>
2018-11-09 16:06:36 -0500

    feature02

--- src/gitmerge/MainApp.java
+++ src/gitmerge/MainApp.java
@@ -4,4 +4,8 @@ public class MainApp {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}
+	
+	public static void featire02() {
+		System.out.println("display feature 02");
+	}
 }
